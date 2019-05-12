c26126f11ac3f76bf4489d82f6ce8cf7c4c47a54
Rex <wlilong@163.com>
2019-05-12 18:28:51 +0800

    update

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+
+mongoose.connect("mongodb+srv://root:1qaz!QAZ@132.232.105.237/test?retryWrites=true", {
+    useNewUrlParser: true
+});
+mongoose.connection.once('open', () => {
+    console.log('connected to database');
+})
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+}));
+
+app.listen(4000, () => {
+    console.log('now listening for requests on port 4000');
+});
